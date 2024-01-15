2b9515b68c02b93297924578781358770c42cc5c
refringerator <refloader@gmail.com>
2024-01-15 16:39:34 +0000

    Deploying to gh-pages from @ refringerator/bugboard@382cacdcc6e3879530addf8dab1dc8eb1f5e0c4e 🚀

--- storybook/assets/preview-saAjfF6A.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./iframe-NsrOE9zG.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-HiTZ55Cd.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]));return new r}}};export{a as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/DocsRenderer-NNNQARDV-HiTZ55Cd.js","assets/iframe-NsrOE9zG.js","assets/index-XiNr8FW2.js","assets/_commonjsHelpers-5-cIlDoe.js","assets/react-18-sFOusbzT.js","assets/index-9vG4XYWr.js","assets/index-Vc7C5wC5.js","assets/index-ogXoivrg.js","assets/index-mLPG47JP.js","assets/index-PPLHz8o0.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
