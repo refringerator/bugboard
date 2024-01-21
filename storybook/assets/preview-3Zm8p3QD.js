fcd5cc3f4fe2ee0c96dddfe39b51194cad361266
refringerator <refloader@gmail.com>
2024-01-21 00:16:51 +0000

    Deploying to gh-pages from @ refringerator/bugboard@ce49762c82c6559bada39080bb32809c9b59c00c 🚀

--- storybook/assets/preview-3Zm8p3QD.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./iframe-dk3shi6l.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-r_zd1V7Z.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]));return new r}}};export{a as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/DocsRenderer-NNNQARDV-r_zd1V7Z.js","assets/iframe-dk3shi6l.js","assets/index-XiNr8FW2.js","assets/_commonjsHelpers-5-cIlDoe.js","assets/react-18-sFOusbzT.js","assets/index-9vG4XYWr.js","assets/index-tleXYYmA.js","assets/index-ogXoivrg.js","assets/index-mLPG47JP.js","assets/index-PPLHz8o0.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
