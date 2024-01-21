9e99458d24f61c86c9569a34e87f82a703971389
petruha <refloader@gmail.com>
2024-01-22 00:42:03 +0300

    Перемещение окна

--- src/components/Window/Window.tsx
+++ src/components/Window/Window.tsx
@@ -1,3 +1,4 @@
+import { useRef, useState, MouseEventHandler } from 'react';
 import './Window.css';
 import useResizeElement from 'src/hooks/useResizeElement';
 
@@ -19,18 +20,54 @@ function Window({
     width: curWidth,
     height: curHeight,
   } = useResizeElement({ minWidth, minHeight, width, height });
+  const [position, setPosition] = useState({ x: 100, y: 100 });
+  const [isGrabbing, setGrabbing] = useState(false);
+
+  const isDragging = useRef(false);
+
+  const handleMouseDown: MouseEventHandler<HTMLDivElement> = (e) => {
+    setGrabbing(true);
+    isDragging.current = true;
+    const offsetX = e.clientX - position.x;
+    const offsetY = e.clientY - position.y;
+
+    const handleMouseMove = (ev: MouseEvent) => {
+      if (isDragging.current) {
+        setPosition({ x: ev.clientX - offsetX, y: ev.clientY - offsetY });
+      }
+    };
+
+    const handleMouseUp = () => {
+      // setDragging(false);
+      isDragging.current = false;
+      window.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener('mouseup', handleMouseUp);
+      setGrabbing(false);
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+    window.addEventListener('mouseup', handleMouseUp);
+  };
 
   return (
     <div
       className="window"
       style={{
-        // position: 'absolute',
+        position: 'absolute',
 
+        left: position.x,
+        top: position.y,
         width: curWidth + 2,
         height: curHeight + 2,
       }}
     >
-      <div className="window__header">header</div>
+      <div
+        className="window__header"
+        onMouseDown={handleMouseDown}
+        style={{ cursor: isGrabbing ? 'grabbing' : 'grab' }}
+      >
+        header
+      </div>
       <div className="window__content" onMouseDown={handleMouseDownResize}>
         content
       </div>
