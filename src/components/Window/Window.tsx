1262b2b73953d59dce1c8de4d4346a2874215ac7
petruha <refloader@gmail.com>
2024-01-21 22:13:35 +0300

    Перенести логику изменения размера в хук

--- src/components/Window/Window.tsx
+++ src/components/Window/Window.tsx
@@ -1,5 +1,5 @@
-import { useState, useRef, MouseEventHandler } from 'react';
 import './Window.css';
+import useResizeElement from 'src/hooks/useResizeElement';
 
 interface MainMenuProps {
   width?: number;
@@ -14,62 +14,26 @@ function Window({
   width = 200,
   height = 150,
 }: MainMenuProps) {
-  const [winSize, setWinSize] = useState({ width, height });
-  const isResizing = useRef(false);
-  const iSize = useRef<{ height: number; width: number; x: number; y: number }>(
-    { height: 0, width: 0, x: 0, y: 0 }
-  );
-
-  const handleMouseMove = (event: MouseEvent) => {
-    if (isResizing.current) {
-      const newWidth = iSize.current.width + (event.screenX - iSize.current.x);
-      const newHeight =
-        iSize.current.height + (event.screenY - iSize.current.y);
-
-      if (!Number.isNaN(newWidth) && !Number.isNaN(newWidth)) {
-        setWinSize({
-          width: Math.max(newWidth, minWidth),
-          height: Math.max(newHeight, minHeight),
-        });
-      }
-    }
-  };
-
-  const handleMouseUp = () => {
-    isResizing.current = false;
-    document.removeEventListener('mousemove', handleMouseMove);
-    document.removeEventListener('mouseup', handleMouseUp);
-  };
-
-  const handleMouseDown: MouseEventHandler<HTMLDivElement> = (event) => {
-    isResizing.current = true;
-
-    iSize.current = {
-      x: event.screenX,
-      y: event.screenY,
-      width: winSize.width,
-      height: winSize.height,
-    };
-
-    document.addEventListener('mousemove', handleMouseMove);
-    document.addEventListener('mouseup', handleMouseUp);
-  };
+  const {
+    handleMouseDown: handleMouseDownResize,
+    width: curWidth,
+    height: curHeight,
+  } = useResizeElement({ minWidth, minHeight, width, height });
 
   return (
     <div
       className="window"
-      //   onMouseEnter={ome}
-      //   onMouseLeave={oml}
-      onMouseDown={handleMouseDown}
-      //   onMouseUp={omu}
       style={{
         // position: 'absolute',
-        width: winSize.width + 2,
-        height: winSize.height + 2,
+
+        width: curWidth + 2,
+        height: curHeight + 2,
       }}
     >
       <div className="window__header">header</div>
-      <div className="window__content">content</div>
+      <div className="window__content" onMouseDown={handleMouseDownResize}>
+        content
+      </div>
     </div>
   );
 }
---
+++ src/hooks/useResizeElement.ts
@@ -0,0 +1,67 @@
+import { useState, useRef, MouseEventHandler } from 'react';
+
+interface MainMenuProps {
+  width?: number;
+  height?: number;
+  minWidth?: number;
+  minHeight?: number;
+}
+
+function useResizeElement({
+  minWidth = 100,
+  minHeight = 80,
+  width = 200,
+  height = 150,
+}: MainMenuProps) {
+  const [winSize, setWinSize] = useState({ width, height });
+  const isResizing = useRef(false);
+  const iSize = useRef<{ height: number; width: number; x: number; y: number }>(
+    { height: 0, width: 0, x: 0, y: 0 }
+  );
+
+  const handleMouseMove = (event: MouseEvent) => {
+    if (isResizing.current) {
+      const newWidth = iSize.current.width + (event.screenX - iSize.current.x);
+      const newHeight =
+        iSize.current.height + (event.screenY - iSize.current.y);
+
+      if (!Number.isNaN(newWidth) && !Number.isNaN(newWidth)) {
+        setWinSize({
+          width: Math.max(newWidth, minWidth),
+          height: Math.max(newHeight, minHeight),
+        });
+      }
+    }
+  };
+
+  const handleMouseUp = () => {
+    isResizing.current = false;
+    document.removeEventListener('mousemove', handleMouseMove);
+    document.removeEventListener('mouseup', handleMouseUp);
+  };
+
+  const handleMouseDown: MouseEventHandler<HTMLDivElement> = (event) => {
+    isResizing.current = true;
+
+    iSize.current = {
+      x: event.screenX,
+      y: event.screenY,
+      width: winSize.width,
+      height: winSize.height,
+    };
+
+    document.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener('mouseup', handleMouseUp);
+  };
+
+  return { handleMouseDown, width: winSize.width, height: winSize.height };
+}
+
+useResizeElement.defaultProps = {
+  minWidth: 100,
+  minHeight: 80,
+  width: 200,
+  height: 150,
+};
+
+export default useResizeElement;
