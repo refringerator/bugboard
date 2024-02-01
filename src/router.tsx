d5fdf45f5234b8cbe422a6d856b4bd48dafa848c
petruha <refloader@gmail.com>
2024-02-01 18:32:37 +0300

    Переделать роутер в хэшный

--- src/router.tsx
+++ src/router.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/jsx-key */
 import {
   createRoutesFromElements,
-  createBrowserRouter,
   Route,
+  createHashRouter,
 } from 'react-router-dom';
 
 import App from './windows/App/App';
@@ -10,46 +10,19 @@ import ErrorPage from './error-page';
 import OAuthCallback from './routes/OAuthCallback';
 import MainScreen from './windows/MainScreen/MainScreen';
 
-const router = createBrowserRouter(
+const router = createHashRouter(
   createRoutesFromElements([
-    <Route
-      path="/"
-      element={<MainScreen />}
-      // loader={OAuthCallback.loader}
-      // action={editAction}
-    />,
-    <Route
-      path="/app"
-      element={<App />}
-      // loader={rootLoader}
-      // action={rootAction}
-      errorElement={<ErrorPage />}
-    >
-      {/* <Route errorElement={<ErrorPage />}>
-        <Route index element={<Index />} />
-        <Route
-          path="contacts/:contactId"
-          element={<Contact />}
-          loader={contactLoader}
-          action={contactAction}
-        />
-        <Route
-          path="contacts/:contactId/edit"
-          element={<EditContact />}
-          loader={contactLoader}
-          action={editAction}
-        />
-        <Route path="contacts/:contactId/destroy" action={destroyAction} />
-      </Route> */}
+    <Route path="/" element={<MainScreen />}>
+      <Route path="app" element={<App />} errorElement={<ErrorPage />} />
+      <Route
+        path="oauth-callback"
+        element={<OAuthCallback />}
+        loader={OAuthCallback.loader}
+      />
     </Route>,
-    <Route
-      path="oauth-callback"
-      element={<OAuthCallback />}
-      loader={OAuthCallback.loader}
-      // action={editAction}
-    />,
-  ]),
-  { basename: import.meta.env.BASE_URL }
+  ])
+  // нужно для BrowserRouter
+  // { basename: import.meta.env.BASE_URL }
 );
 
 export default router;
