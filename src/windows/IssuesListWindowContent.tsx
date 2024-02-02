e3cd5d49e91d225d8cb17912fa17f3dd8223e2a4
petruha <refloader@gmail.com>
2024-02-02 03:24:45 +0300

    Добавить подгрузку элементов в окно списка задач

--- src/windows/IssuesListWindowContent.tsx
+++ src/windows/IssuesListWindowContent.tsx
@@ -1,7 +1,43 @@
-function IssuesListWindowContent() {
-  const title = 'ISSUES LIST WINDOW';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { Issue, useGetPostsQuery } from 'src/service/issues';
 
-  return <div>{title}</div>;
+function PostListItem({
+  data: { name, id },
+  onSelect,
+}: {
+  data: Issue;
+  onSelect: (id: number) => void;
+}) {
+  return (
+    <li>
+      <a href="#" onClick={() => onSelect(id)}>
+        {name}
+      </a>
+    </li>
+  );
+}
+function IssuesListWindowContent() {
+  const { data: posts, isLoading } = useGetPostsQuery();
+
+  if (isLoading) {
+    return <div>Loading</div>;
+  }
+
+  if (!posts) {
+    return <div>No issues :(</div>;
+  }
+
+  return (
+    <div>
+      {posts.map((post) => (
+        <PostListItem
+          key={post.id}
+          data={post}
+          onSelect={(id) => console.log(`/posts/${id}`)}
+        />
+      ))}
+    </div>
+  );
 }
 
 IssuesListWindowContent.windowId = 'IssuesListWindow';
