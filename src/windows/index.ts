e8988689184b517a10f5c62d42b8975364c5c3fe
petruha <refloader@gmail.com>
2024-02-06 18:31:02 +0300

    Перенести окна в ишузы

--- src/windows/index.ts
+++ src/windows/index.ts
@@ -1,7 +1,7 @@
 export { default as MainScreen } from './MainScreen/MainScreen';
 export { default as Background } from './Background';
 export { default as CounterWindowContent } from './CounterWindowContent';
-export { default as IssueWindowContent } from './IssueWindowContent';
-export { default as IssuesListWindowContent } from './IssuesListWindowContent';
+export { default as IssueWindowContent } from './Issues/IssueWindowContent';
+export { default as IssuesListWindowContent } from './Issues/IssuesListWindowContent';
 export { default as SettingsWindowContent } from './SettingsWindowContent';
 export { default as App } from './App/App';
