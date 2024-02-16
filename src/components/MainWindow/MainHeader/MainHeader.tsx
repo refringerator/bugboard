da568f299164d28ed8339c25e36a07e71a3bc90c
petruha <refloader@gmail.com>
2024-02-16 21:13:39 +0300

    Добавить переключение фуллскрина

--- src/components/MainWindow/MainHeader/MainHeader.tsx
+++ src/components/MainWindow/MainHeader/MainHeader.tsx
@@ -7,12 +7,27 @@ interface ButtonProps {
 }
 
 function MainHeader({ onClick, title, icon }: ButtonProps) {
+  const toggleFullscreen = () => {
+    if (!document.fullscreenElement) {
+      document.documentElement.requestFullscreen();
+    } else if (document.exitFullscreen) {
+      document.exitFullscreen();
+    }
+  };
+
   return (
     <div className="row header">
       <img className="header__icon" src={icon} alt="" />
       <span className="header__text">{title}</span>
       <button type="button" onClick={onClick} className="header__button">
         X
+      </button>
+      <button
+        type="button"
+        onClick={toggleFullscreen}
+        className="header__button"
+      >
+        O
       </button>
     </div>
   );
